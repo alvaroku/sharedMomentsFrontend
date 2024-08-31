051cf5880b33a57da876f1e07904f604f52ead9b
Alvaro Ku <alvaro.ku@grupoblueocean.com>
2024-08-31 17:33:08 -0600

    feat

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -17,6 +17,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     MessageService,
     DialogService,
-    ConfirmationService
+    ConfirmationService,
+    // {provide:LOCALE_ID, useValue: 'es' }
   ]
 };
